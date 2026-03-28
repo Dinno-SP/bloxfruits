22288b432a816dfdc4c16020f0793830b11bbec0
Dinno-SP <denispinerpotu1@gmail.com>
2026-03-28 12:32:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Terinstal');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('[Service Worker] Aktif');
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-    // Memenuhi syarat Google Chrome agar bisa diinstall
+    // Mesin jalan agar Chrome mengizinkan Install
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            console.log('Mode Offline');
+        })
+    );
 });
